cb4db5c0e667f2484021524d7530beb730b75265
nitish1996 <nitishsharma1809@gmail.com>
2017-07-16 15:26:02 -0400

    updated

---
+++ spy_details.py
@@ -0,0 +1,21 @@
+#use this "lewis" and "1234" in user-name and password
+loginun="nitish"                      #variable taken
+loginps="1234"
+def details():
+    while 1 == 1:
+        print "Enter your username:"  # created username
+        logi = raw_input()
+        if logi==loginun:
+            print "Enter your password:"  # created password
+            login = raw_input()
+            if login==loginps:
+                print "login sucessful:!!!"
+                break;
+
+            else:
+                print"Password incorrect"
+
+        else:
+            print "login failed!!!\t re-enter"
+
+    print "wel come back\n\t Mr Nitish\n\tage-20\n\trating-4(good)\n\tyou are online * now"
